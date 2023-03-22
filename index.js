c63ed9dc48010cfb7f4e316676023096be246696
mauger1998 <110930037+mauger1998@users.noreply.github.com>
2023-03-22 09:30:50 +0000

    Add cms functionality

--- index.js
+++ index.js
@@ -15,3 +15,38 @@ document.addEventListener("click", (e) => {
         dropdown.classList.remove("active")
     })
 })
+
+// Cms
+
+let URL = "https://c82a0tvp.api.sanity.io/v2021-10-21/data/query/production?query=*%5B_type%20%3D%3D%20%22beforeAfterImage%22%5D%20%7B%0A%20%20%0A%20%20%22imageUrl%22%3A%20beforeAfterImage.asset-%3Eurl%0A%7D%0A"
+
+fetch(URL)
+        .then((res) => res.json())
+        .then(({ result }) => {
+          const beforeAfterImg = document.querySelectorAll(".section-four-image img")
+
+            result.forEach((result, index) => {
+                    beforeAfterImg[index].src = result.imageUrl
+            });
+            
+        })
+        .catch((err) => console.error(err));
+
+let URLTWO = "https://c82a0tvp.api.sanity.io/v2021-10-21/data/query/production?query=*%5B_type%20%3D%3D%20%22quote%22%5D%20%0A"
+
+fetch(URLTWO)
+        .then((res) => res.json())
+        .then(({ result }) => {
+          const quote = document.querySelector(".section-five > .wrapper > h2")
+          console.log(quote)
+
+          console.log(result)
+
+          quote.textContent = result[0].quote
+
+            
+        })
+        .catch((err) => console.error(err));
+
+
+
--- testimonials/index.js
+++ testimonials/index.js
@@ -15,3 +15,23 @@ document.addEventListener("click", (e) => {
         dropdown.classList.remove("active")
     })
 })
+
+// Cms
+
+let URL = "https://c82a0tvp.api.sanity.io/v2021-10-21/data/query/production?query=*%5B_type%20%3D%3D%20%22testimonial%22%5D%20%0A"
+
+fetch(URL)
+        .then((res) => res.json())
+        .then(({ result }) => {
+          const testimonialCards = document.querySelectorAll(".section-two-card")
+
+          result.forEach((result, index) => {
+            console.log(testimonialCards[index].firstChild)
+            testimonialCards[index].children[0].textContent = result.name
+            testimonialCards[index].children[1].textContent = result.review
+          })
+          
+
+            
+        })
+        .catch((err) => console.error(err));
